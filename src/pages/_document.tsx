2b1b52405c701c600454960e49df4a819a38be14
John Freitas <john.freitasau@gmail.com>
2021-01-22 17:41:05 +1100

    chore: Updated favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;700;800&display=swap" rel="stylesheet" />
 
-          <link rel='icon' href='https://rocketseat.com.br/favicon.ico' />
+          <link rel='icon' href='/assets/favicon.ico' />
 
         </Head>
         <body>
